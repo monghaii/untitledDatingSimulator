f97ab54f67cdec838216ecead6b9b1029a8437f0
Lin She <linshe@usc.edu>
2024-02-22 15:06:46 -0800

    Adding utility functions to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using Yarn.Unity;
 using UnityEngine.UI;
@@ -9,13 +11,19 @@ public class GameManager : MonoBehaviour
 {
     [Header("Scene Management")] 
     public Canvas datingSimInterface;
+    public EventSystem datingSimEventSystem;
     private bool fpsLoaded = false;
     
     [Header("Characters")] 
     public Image characterImage;
     public Characters characterSO;
-    
 
+    // This should be populated in Unity Editor
+    // Variable to store different ending screens
+    // string: The name of the ending screen (for function call)
+    // GameObject: The actual object of the ending screen (like an image)
+    [Header("EndScreens")] public Dictionary<string, GameObject> EndScreens;
+    
     void Update()
     { 
         // this is just for testing additive scene loading
@@ -32,6 +40,10 @@ public class GameManager : MonoBehaviour
         
         // hides dating sim ui
         datingSimInterface.enabled = false;
+        characterImage.enabled = false;
+        
+        // handles event system control over to FPS scene
+        datingSimEventSystem.enabled = false;
 
         // load in fps scene
         Cursor.lockState = CursorLockMode.Locked; // also done in the FirstPersonCamera script but here again just in case
@@ -43,7 +55,11 @@ public class GameManager : MonoBehaviour
         
         // enable dating sim ui
         datingSimInterface.enabled = true;
+        characterImage.enabled = true;
 
+        // handles event system control back
+        datingSimEventSystem.enabled = true;
+        
         // unload fps scene
         Cursor.lockState = CursorLockMode.None;
         SceneManager.UnloadSceneAsync("FPSScene");
@@ -72,6 +88,29 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // Function that can be called by Yarn to change game view
+    [YarnCommand("ChangeMode")]
+    public void ChangeMode(bool fpsMode)
+    {
+        if (fpsMode)
+        {
+            StartFPS();
+        } else
+        {
+            EndFPS();
+        }
+    }
+
+    // This is a Yarn-callable function that shows screen if existed by name
+    [YarnCommand("ShowEndScreen")]
+    public void ShowEndScreen(string screenName)
+    {
+        if (EndScreens.TryGetValue(screenName, out var screenObj))
+        {
+            screenObj.SetActive(true);
+        }
+    }
+    
     public void ReloadGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
